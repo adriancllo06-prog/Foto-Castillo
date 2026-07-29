44d6c24a1664f476993c2cffa91281a40b9d82c9
Adrian Castillo <Adriancllo06@gmail.com>
2026-07-28 20:18:53 -0400

    Actualiza Contenido de App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>ambar comercial</h1>
       <h1>Con Dios todo es posible</h1>
+      <h1>con dios todo se puede , amen</h1>
     </div>  
 
     
