8de4610928bc66d1e0b786d5bcccb6e28326229a
Adrian Castillo <Adriancllo06@gmail.com>
2026-08-04 17:57:50 -0400

    Estructura inicial

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,31 @@ function App() {
   
 
   return (
-    <div className="App">
-      <h1>ambar comercial</h1>
-      <h1>Con Dios todo es posible</h1>
-      <h1>con dios todo se puede , amen</h1>
-    </div>  
+ <>
+      <header>
+        <h1>Foto Castillo</h1>
+
+        <nav>
+          <a href="#">Inicio</a>
+          <a href="#">Portafolio</a>
+          <a href="#">Servicios</a>
+          <a href="#">Contacto</a>
+        </nav>
+      </header>
+
+      <main>
+        <section>
+          <h2>Capturando momentos únicos</h2>
+          <p>
+            Bienvenido a mi portafolio de fotografía. Aquí encontrarás algunos
+            de mis mejores trabajos.
+          </p>
+
+          <button>Ver Portafolio</button>
+        </section>
+      </main>
+    </>
+    
 
     
       
