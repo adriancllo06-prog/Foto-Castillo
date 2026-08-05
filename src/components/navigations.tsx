57bb58bf556039bfc2cfb231a62a26df8d4ef7ba
Adrian Castillo <Adriancllo06@gmail.com>
2026-08-04 20:37:44 -0400

    refactor: añadido componentes navegacion

---
+++ src/components/navigations.tsx
@@ -0,0 +1,12 @@
+function Navigation() {
+  return (
+    <nav>
+      <a href="#">Inicio</a>
+      <a href="#">Portafolio</a>
+      <a href="#">Servicios</a>
+      <a href="#">Contacto</a>
+    </nav>
+  )
+}
+
+export default Navigation
