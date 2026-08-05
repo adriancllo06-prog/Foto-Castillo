8f835ae29909b989f47bae03b4afa83ebd0c4226
Adrian Castillo <Adriancllo06@gmail.com>
2026-08-04 21:12:09 -0400

    imagen agregada

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-
+import Navigation from './components/navigations';
+import assets from './assets/DSC_0090.jpg';
 function App() {
   
 
@@ -7,22 +8,18 @@ function App() {
       <header>
         <h1>Foto Castillo</h1>
 
-        <nav>
-          <a href="#">Inicio</a>
-          <a href="#">Portafolio</a>
-          <a href="#">Servicios</a>
-          <a href="#">Contacto</a>
-        </nav>
+        <Navigation />
       </header>
     
       <main>
         <section>
           <h2>Capturando momentos únicos</h2>
+
           <p>
             Bienvenido a mi portafolio de fotografía. Aquí encontrarás algunos
             de mis mejores trabajos.
           </p>
-
+      <img src={assets} alt="Foto de ejemplo" width="500px" height="500px"/>
           <button>Ver Portafolio</button>
         </section>
       </main>
